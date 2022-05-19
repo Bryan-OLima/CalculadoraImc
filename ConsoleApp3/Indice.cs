55f4b823158c39f735504884544b639aef1901be
Bryan-OLima <bryanosoriolima@gmail.com>
2022-05-18 21:54:19 -0300

    first Commit

---
+++ ConsoleApp3/Indice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Indice
+    {
+        double Altura { get; set; }
+        double Peso { get; set; }
+
+        public Indice(double peso, double altura)
+        {
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public double Imc(double peso, double altura)
+        {
+            double imc;
+            imc = Peso / (Altura * Altura);
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Muito magro!");
+            }
+            else if (imc < 25)
+            {
+                Console.WriteLine("Peso ideal!");
+            }
+            else if (imc < 30)
+            {
+                Console.WriteLine("Obesidade!");
+            }
+            else if (imc < 40)
+            {
+                Console.WriteLine("Obesidade grau 2!");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade mórbida!");
+            }
+
+            return imc;
+        }
+
+        public void Zerar(double valor1, double valor2)
+        {
+            Peso = valor1 - valor1;
+            Altura = valor2 - valor2;
+        }
+        public override string ToString()
+        {
+            return "Seu peso é: " +
+                Peso +
+                " Sua altura é: " +
+                Altura;                
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ConsoleApp3;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Deseja fazer uma consulta? [S/N]: ");
+            string resposta = Console.ReadLine().ToLower();
+
+            while (resposta == "s")
+            {
+                Console.Write("peso: ");
+                double peso = double.Parse(Console.ReadLine());
+
+                Console.Write("Altura: ");
+                double altura = double.Parse(Console.ReadLine());
+
+                Indice pessoa = new Indice(peso, altura);
+
+                Console.WriteLine("Seu imc é: " + pessoa.Imc(peso, altura).ToString("F2"));
+                pessoa.Zerar(peso, altura);
+                
+                Console.WriteLine();
+                Console.Write("Deseja fazer uma nova consulta? [S/N]: ");
+                resposta = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Obrigado por usar nosso sistema de IMC!");
+        }
+
+
+    }
+}
